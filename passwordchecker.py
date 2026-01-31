7f1d0f796a50757a6366f5a658f3d748b95f97ac
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2026-02-01 05:23:28 +0530

    wrote_base_code

--- passwordchecker.py
+++ passwordchecker.py
@@ -1,1 +1,28 @@
-print("hello world")
+# password-strength-analyzer
+import re
+# checking conditions : 
+# min 8 characters,digits,uppercase,lowercase,special characters
+def check_password_strength(password):
+    if len(password)<8: # checking length of the password
+        return "Weak:password must be of 8 characters"
+    if not any(char.isdigit()for char in password):
+        return "Weak:password must contain a digits"
+    if not any(char.isupper() for char in password):
+        return "Weak:password must contain an uppercase character"
+    if not any(char.islower() for char in password):
+        return "Weak:password must contain a lowercase character"
+    if not re.search(r"[@#$%&!^()<>.?]",password):
+        return "Medium:password must contain a special character"
+    return "Strong:Your password is strong"
+def password_checker():
+    print("welcome to password-strength-analyzer")
+    while(True):
+        password=input("please enter your password(or type 'exit' to quit):\n")
+        if password.lower()=='exit':
+            print("Thank you for using this tool\n")
+            break
+        result=check_password_strength(password)
+        print(result)
+# Running the password-analyzer tool
+if __name__=="__main__": 
+    password_checker()
